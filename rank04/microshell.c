903430c4bd7eaf5f8033553585a166513380b7a0
Charlotte LE GRAND <cle-gran@e2r3p17.clusters.42paris.fr>
2022-06-02 18:55:32 +0200

    c est mieux comme ca

--- rank04/microshell.c
+++ rank04/microshell.c
@@ -6,19 +6,20 @@ int ft_putstr_fd2(char *str, char *arg)
 {
     while (*str)
         write(2, str++, 1);
+    
     if (arg)
         while (*arg)
             write(2, arg++, 1);
     write(2, "\n", 1);
-    return (1);
+    return (0);
 }
 
 int ft_execute(char **av, int i, int tmp_fd, char **env)
 {
+    av[i] = NULL;
     close(tmp_fd);
-    av[i]= NULL;
     execve(av[0], av, env);
-    return (ft_putstr_fd2("error: cannot execute ", av[0]));
+    return (ft_putstr_fd2("pb d'exec", av[0]));
 }
 
 int main(int ac, char **av, char **env)
@@ -32,20 +33,20 @@ int main(int ac, char **av, char **env)
     i = 0;
     pid = 0;
     tmp_fd = dup(STDIN_FILENO);
-    while(av[i] && av[i+1])
+    while (av[i] && av[i + 1])
     {
         av = &av[i + 1];
         i = 0;
-        while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
+        while (av[i] && strcmp(av[i],"|") && strcmp(av[i], ";"))
             i++;
         if (strcmp(av[0], "cd") == 0)
         {
-            if (i != 2)
-                ft_putstr_fd2("error: cd: bad arguments", NULL);
+            if (i!=2)
+                ft_putstr_fd2("mauvais args cd", NULL);
             else if (chdir(av[1]) != 0)
                 ft_putstr_fd2("error: cd: cannot change directory to ", av[1]);
         }
-        else if (i != 0 && (av[i] == NULL || strcmp(av[i], ";") == 0))
+        else if (i!= 0 && (av[i] == NULL || strcmp(av[i], ";") == 0))
         {
             pid = fork();
             if (pid == 0)
@@ -54,7 +55,7 @@ int main(int ac, char **av, char **env)
                 if (ft_execute(av, i, tmp_fd, env))
                     return (1);
             }
-            else
+            else 
             {
                 close(tmp_fd);
                 while (waitpid(-1, NULL, WUNTRACED) != -1)
@@ -75,10 +76,10 @@ int main(int ac, char **av, char **env)
                 if (ft_execute(av, i, tmp_fd, env))
                     return (1);
             }
-            else
+            else 
             {
-                close(fd[0]);
                 close(fd[1]);
+                close(tmp_fd);
                 tmp_fd = fd[0];
             }
         }
